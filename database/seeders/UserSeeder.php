0a4e9bc0efbc6da667bb65782ed9f06483bdbeb5
code-arif <arifulislam6460@gmail.com>
2025-10-26 09:23:06 +0600

    100 use seeder data add

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -35,68 +36,26 @@ class UserSeeder extends Seeder
             'is_apple_signin' => false,
         ]);
 
-        // --- 3 Regular Users ---
-        $users = [
-            [
-                'first_name' => 'Arif',
-                'last_name' => 'Hossain',
-                'email' => 'a@gmail.com',
-                'phone' => '0987654323',
-            ],
-            [
-                'first_name' => 'john',
-                'last_name' => 'doe',
-                'email' => 'john@doe.com',
-                'phone' => '09876543230',
-            ],
-            [
-                'first_name' => 'jimmi',
-                'last_name' => 'doe',
-                'email' => 'jimmi@doe.com',
-                'phone' => '098765432003',
-            ],
-            [
-                'first_name' => 'rahat',
-                'last_name' => 'khan',
-                'email' => 'rahat@khan.com',
-                'phone' => '098760054323',
-            ],
-            [
-                'first_name' => 'rabbi',
-                'last_name' => 'alam',
-                'email' => 'rabbi@alam.com',
-                'phone' => '098768854323',
-            ],
-            [
-                'first_name' => 'hossain',
-                'last_name' => 'ali',
-                'email' => 'hossain@ali.com',
-                'phone' => '098765432308',
-            ],
-            [
-                'first_name' => 'sandam',
-                'last_name' => 'hossain',
-                'email' => 'sadman@hossain.com',
-                'phone' => '098765485323',
-            ],
-            [
-                'first_name' => 'sagar',
-                'last_name' => 'mahmud',
-                'email' => 'sagar@mahmud.com',
-                'phone' => '0987654840323',
-            ],
-        ];
+        // --- 100 Regular Users ---
+        $faker = Faker::create();
 
-        foreach ($users as $user) {
+        for ($i = 1; $i <= 100; $i++) {
+            $firstName = $faker->firstName;
+            $lastName = $faker->lastName;
+            $email = strtolower($firstName . '.' . $lastName . $i . '@example.com');
+
             User::create([
-                ...$user,
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'username' => strtolower($firstName . $lastName . $i),
                 'role' => 'user',
-                'username' => strtolower($user['first_name'] . $user['last_name']),
+                'email' => $email,
+                'phone' => '01' . $faker->numberBetween(100000000, 999999999),
                 'password' => Hash::make('12345678'),
-                'avatar' => 'default/default_person.jpg',
-                'cover' => 'default/default_cover.jpg',
-                'bio' => 'Hello! I’m ' . $user['first_name'] . '.',
-                'address' => 'Dhaka, Bangladesh',
+                'avatar' => asset('default/default_image.jpg'),
+                'cover' => asset('default/default_image.jpg'),
+                'bio' => 'Hello! I’m ' . $firstName . '.',
+                'address' => $faker->city . ', Bangladesh',
                 'otp_verified_at' => Carbon::now(),
                 'status' => 'active',
                 'is_google_signin' => false,
